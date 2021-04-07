a0c13faef242f2bfc4df2cd400823068773a72c7
Ahmed <yghnam334455@gmail.com>
2021-04-07 03:41:06 +0200

    navagationBar

--- app/src/main/java/com/example/studentadmin/MainActivity.java
+++ app/src/main/java/com/example/studentadmin/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.studentadmin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+    private ChipNavigationBar chipNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // id
+        chipNavigationBar = findViewById(R.id.bottom_nav_bar);
+        //
+//kkk
+
+        // NavigationBar
+        chipNavigationBar.setItemSelected(R.id.nav_post, true);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.nav_host_fragment, new PostsFragment()).commit();
+        bottomMenu();
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(i -> {
+            Fragment fragment = null;
+            switch (i) {
+                case R.id.nav_post:
+                    fragment = new PostsFragment();
+                    break;
+                case R.id.nav_quaiz:
+                    fragment = new QuaizFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment
+                    , fragment).commit();
+        });
     }
 }
---
+++ app/src/main/java/com/example/studentadmin/QuaizFragment.java
@@ -0,0 +1,35 @@
+package com.example.studentadmin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class QuaizFragment extends Fragment {
+
+
+
+    public QuaizFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quaiz, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
